64e7c85a3dcaed1d88f9afe05b1988f0cb41ec53
allyson <allyson.brandao@venhapranuvem.com.br>
2020-11-10 08:10:03 -0300

    Aprendendo sobre comentários.

---
+++ Aullas/Aula_04/comentarios.js
@@ -0,0 +1,14 @@
+// Escrever cmentario
+
+console.log('Hello World !'); // Aqui tem outro comentário
+
+// Linhas
+
+console.log('Linha 4 !');
+
+/* 
+    console.log('Linha 5 !');
+    console.log('Linha 6 !'); 
+*/
+
+// Vai ser ignorado
