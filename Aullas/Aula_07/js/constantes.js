39946253e7bf92ad8cc9a4b7444fb43fe9a4314c
allyson <allyson.brandao@venhapranuvem.com.br>
2020-11-10 09:31:39 -0300

    Constantes com const e correção de exercicios.

---
+++ Aullas/Aula_07/js/constantes.js
@@ -0,0 +1,17 @@
+// + - * /
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+const resultado = primeiroNumero * segundoNumero;
+
+const resultadoDuplicado = resultado * 2;
+
+let resultadoTriplicado = resultado * 3;
+
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(primeiroNumero)
+console.log(segundoNumero)
+console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
---
+++ Aullas/Aula_07/js/dist/constantes.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// + - * /
+var primeiroNumero = 5;
+var segundoNumero = 10;
+var resultado = primeiroNumero * segundoNumero;
+var resultadoDuplicado = resultado * 2;
+var resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(primeiroNumero);
+console.log(segundoNumero);
+console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
