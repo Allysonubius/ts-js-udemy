7ee03ac9effc17384a3edf88ed5384d9ad8906a1
allyson <allyson.brandao@venhapranuvem.com.br>
2020-11-10 09:02:19 -0300

    Variaveis com let

---
+++ Aullas/Aula_06/dist/variaveis.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//primeira forma atualizada de se usar uma variavel
+var name = 'João'; // var name = 'João'; //forma antiga de declarar uma variavel
+//espaço
+
+console.log('\n', 'Historia de', name);
+console.log(name, 'nasceu em 1984.');
+console.log('Em 2000', name, 'conheceu Maria.');
+console.log(name, 'casou-se com Maria em 2012.');
+console.log('Maria teve um filho com ', name, 'em 2015.');
+console.log('O filho de', name, 'se chama Eduardo.');
---
+++ Aullas/Aula_06/variaveis.js
@@ -0,0 +1,13 @@
+//primeira forma atualizada de se usar uma variavel
+let name = 'João';
+
+// var name = 'João'; //forma antiga de declarar uma variavel
+
+//espaço
+console.log('\n', 'Historia de', name);
+
+console.log(name, 'nasceu em 1984.');
+console.log('Em 2000', name, 'conheceu Maria.');
+console.log(name, 'casou-se com Maria em 2012.');
+console.log('Maria teve um filho com ', name, 'em 2015.');
+console.log('O filho de', name, 'se chama Eduardo.')
