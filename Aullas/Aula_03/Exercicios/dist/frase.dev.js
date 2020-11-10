60c329e143c890247dff0d7138c4c08b4eacace8
allyson <allyson.brandao@venhapranuvem.com.br>
2020-11-10 07:56:46 -0300

    Exercicios do console.log

---
+++ Aullas/Aula_03/Exercicios/dist/frase.dev.js
@@ -0,0 +1,3 @@
+"use strict";
+
+console.log('Meu nome é ' + 'Allyson. ' + 'Estou aprendendo JS ás', 10, 'da manhã.');
---
+++ Aullas/Aula_03/Exercicios/frase.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é ' + 'Allyson. ' + 'Estou aprendendo JS ás', 10, 'da manhã.')
