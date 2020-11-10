62e80e1de3816bd45f4eefcd521188a0344bf7bd
allyson <allyson.brandao@venhapranuvem.com.br>
2020-11-10 12:20:58 -0300

    Estudando diferenciação entre var e let.

---
+++ Aullas/Aula_09/js/dist/let-var.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// ES6 - Ecmascript 2015
+var nome = 'Lui'; // Erro de declaração duplicata
+
+var n;
---
+++ Aullas/Aula_09/js/let-var.js
@@ -0,0 +1,6 @@
+// ES6 - Ecmascript 2015
+var nome = 'Lui'; // Erro de declaração duplicata.
+
+let nome = 'Souza'; //Correção da duplicidadede declaração.
+
+// Rode para verificar erro.
