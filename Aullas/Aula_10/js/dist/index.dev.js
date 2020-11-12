e5b6a53d987d358a12274323baf6a8d3146e72a9
allyson <allyson.brandao@venhapranuvem.com.br>
2020-11-12 11:44:34 -0300

    Tipos de dados primitivos.

---
+++ Aullas/Aula_10/js/dist/index.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var nome = 'Allyson'; // String com aspas simples
+
+var nome_1 = 'Allyson'; // String com aspas duplas
+
+var nome_2 = "Allyson "; // String com crases
+
+var num = 10; // number
+
+var num_1 = 10.52; // number tipo flutuante
+
+var nomeAluno; // undefined -> não aponta para local nenhum na memória.
+
+var sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória.
+
+var aprovado = false; // Boolean = true, false (lógico).
---
+++ Aullas/Aula_10/js/index.js
@@ -0,0 +1,11 @@
+const nome = 'Allyson'; // String com aspas simples
+const nome_1 = 'Allyson'; // String com aspas duplas
+const nome_2 = `Allyson `; // String com crases
+
+const num = 10; // number
+const num_1 = 10.52 // number tipo flutuante
+
+let nomeAluno; // undefined -> não aponta para local nenhum na memória.
+let sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória.
+
+const aprovado = false; // Boolean = true, false (lógico).
